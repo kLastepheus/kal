8e167fef74ea1106cf63518921daddc4f6edee11
kLastepheus <67367273+kLastepheus@users.noreply.github.com>
2024-12-27 09:16:12 +0300

    testtt.js

---
+++ testtt.js
@@ -0,0 +1,13 @@
+        const element = document.querySelector('.widget-main');
+            if (element) {
+                element.remove();
+            } else {
+                alert('Element zaten silinmiş.');
+            }
+        
+        const element1 = document.querySelector("body > div:nth-child(4) > div > div.col-md-4");
+            if (element1) {
+                element1.remove();
+            } else {
+                alert('Element zaten silinmiş.');
+            }
